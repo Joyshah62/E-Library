618b68fc7d51d797121ba817150110e7d2850abd
Joyshah62 <21cs062@charusat.edu.in>
2022-12-24 00:56:53 +0530

    modified:   package-lock.json 	modified:   package.json 	modified:   src/App.css 	modified:   src/App.js 	deleted:    src/App.test.js 	new file:   src/Header.css 	new file:   src/Header.js 	deleted:    src/logo.svg 	deleted:    src/setupTests.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-
+import { BrowserRouter as Router, Switch , Route} from "react-router-dom";
+import Header from './Header';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+          <div className="app">
+            <Switch>
+                <Route path="/login">
+                  <h1>login</h1>
+                  </Route>
+                  <Route path="/mybooks">
+                    <h1>MyBooks</h1>
+                  </Route>
+                  <Route path="/about">
+                    <h1>About</h1>
+                  </Route>
+                  <Route path="/">
+                    <Header/>
+                  </Route>
+            </Switch>
+          </div>
+    </Router>
+    
   );
 }
 
---
+++ src/Header.js
@@ -0,0 +1,56 @@
+import React from 'react'
+import "./Header.css"
+import { Link } from 'react-router-dom'
+import { FaSearch,FaBookmark } from 'react-icons/fa'
+function Header() {
+  return (
+    <nav className="header">
+        {/*logo on the left -> img */}
+        <Link to="/">
+        <img className='header__logo' src="https://www.concreteisbetter.com/wp-content/uploads/2019/10/elibrary2.png"
+        alt=''
+        />
+        </Link>
+
+        {/* Search box*/}
+        <div className="header__search">
+            <input type="text" className="header__searchInput" />
+            <FaSearch className="header__searchIcon"/>
+        </div>
+
+        {/* 3 links */}
+        <div className='header__nav'>
+
+           <Link to="/" className='header__link'>
+            <div className='header__option'>
+                <span className='button'>Home</span>
+            </div>
+
+           </Link>
+           <Link to="/mybooks" className='header__link'>
+            <div className='header__option'>
+                <span className='button'>MyBooks</span>
+            </div>
+           </Link>
+
+           <Link to="/about" className='header__link'>
+            <div className='header__option'>
+                <span className='button'>About</span>
+            </div>
+           </Link>
+
+           <Link to="/mybooks" className='header__link'>
+            <div className='header__optionBookmark'>
+                <FaBookmark/>
+                <span>0</span>
+
+
+            </div>
+           </Link>
+        </div>
+
+    </nav>
+  )
+}
+
+export default Header
