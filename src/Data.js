f9e470045acd5cad9fbe089bcd445badc07581ce
Joyshah62 <21cs062@charusat.edu.in>
2023-01-11 23:16:18 +0530

    modified:   src/Carousel.css

--- src/Data.js
+++ src/Data.js
@@ -2,22 +2,21 @@ export const countries = [
     
     {
         image: "\\images\\1.png",
-        title: "bg0",
     },
    
     {
         image: "\\images\\2.png",
-        title: "bg1",
+        
     },
     
     {
         image: "\\images\\3.png",
-        title: "bg2",
+        
     },
 
     {
         image: "\\images\\4.png",
-        title: "bg3",
+        
     },
     
 ];
