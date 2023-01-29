4bf6141263196640de49cbd8fc076efda7923ae8
Joyshah62 <21cs062@charusat.edu.in>
2023-01-29 22:04:59 +0530

    added some faqs

--- src/api.js
+++ src/api.js
@@ -9,5 +9,15 @@ export const questions = [
         question: 'Can you download a book without logging in?',
         answer:'No, You must be logged in to download a book.'
     },
+    {
+        id: 3,
+        question: 'What is an eBook?',
+        answer: 'The term eBook stands for electronic book. It is just a book in digital form. This book can be read on electronic book reading devices, as well as tablets, smartphones, laptops, and desktop computers.'
+    },
+    {
+        id: 4,
+        question: 'What are benefits of using eBook?',
+        answer:'Store many books in one device. Read books anywhere anytime. Save storage space. Travel Light. Carry the world of books with you. Quick to download. Instantly available worldwide.'
+    },
     
 ]
